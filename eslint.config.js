1e44bb7073564b14419a9ad49c806b583e128b72
Falk33n <tim.falk00@gmail.com>
2024-12-22 09:36:55 +0100

    chore: update config

--- eslint.config.js
+++ eslint.config.js
@@ -3,13 +3,10 @@ import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
-import { fileURLToPath } from 'node:url';
 import ts from 'typescript-eslint';
 
-const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
-
 const config = ts.config(
-	includeIgnoreFile(gitignorePath),
+	includeIgnoreFile('./.gitignore'),
 	js.configs.recommended,
 	...ts.configs.recommended,
 	...svelte.configs['flat/recommended'],
